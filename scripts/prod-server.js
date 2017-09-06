a3bc1f74da96dcdc54bb1864ce6c950a97ecd902
Anatoly Belonog <anatolybelonog@gmail.com>
2017-09-07 00:17:29 +0700

    fix start script

--- scripts/prod-server.js
+++ scripts/prod-server.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const express = require('express');
+const proxy = require('http-proxy-middleware');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const app = express();
 
 const staticPath = path.join(path.dirname(__dirname), 'browser-bundle');
-console.log(staticPath)
 app.use(express.static(staticPath));
+app.use('/api', proxy({target: 'http://localhost:3000'}))
 
 app.listen(port, (err) => {
   if (err) {
