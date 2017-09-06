68f7c8cebe9cde4e206a18788474529b2b89db2d
Anatoly Belonog <anatolybelonog@gmail.com>
2017-09-07 00:18:01 +0700

    minor fix router

--- src/client/router.tsx
+++ src/client/router.tsx
@@ -3,18 +3,22 @@ import { Router, Route, Redirect, browserHistory } from 'react-router';
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
 
-import {Home} from './pages/home'
-import {Compartments} from './pages/compartments'
+import {Home} from './pages/home';
+import {Compartments} from './pages/compartments';
 
-//<Route path="/home" component={Home} />
-
-export const AppRouter = () => (
-  <Provider store={store}>
-  <Router history={browserHistory}>
+const routes = (
+  <Route>
     <Route path="/compartments" component={Compartments}/>
     <Route path="/home" component={Home}/>
     <Redirect from='/' to='/compartments' />
     <Redirect from='*' to='/' />
-  </Router>
+  </Route>
+);
+
+export const AppRouter = () => (
+  <Provider store={store}>
+    <Router history={browserHistory} createElement={this.createElement}>
+      {routes}
+    </Router>
   </Provider>
 );
