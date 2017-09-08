2e854738a66e49c9363e16dcf5861553947df97a
Anatoly Belonog <anatolybelonog@gmail.com>
2017-09-08 17:17:40 +0700

    refactor plots

--- src/client/pages/compartments/components/identifiability/identifiability-plot.tsx
+++ src/client/pages/compartments/components/identifiability/identifiability-plot.tsx
@@ -1,50 +1,19 @@
 import { IIdentifiabilitySolution } from '../../../../../server/modules/solver/identify-solver';
+import { ParametersPlot } from '../plots/ParametersPlot';
 import * as React from 'react';
-import * as _ from 'lodash';
-const {BarChart, Bar, XAxis, YAxis, Tooltip, Legend} = require('recharts');
 
 interface IProps {
     solution: IIdentifiabilitySolution;
 }
 
 interface IState {
-    data: ISolutionData[];
-}
-
-interface ISolutionData {
-    name: string;
-    value: number;
-}
-
-function configureChartData(solution: IIdentifiabilitySolution): ISolutionData[] {
-    return _.map(solution.solution, (value, key) => {
-        return {name: key, value};
-    });
 }
 
 export class IdentifiabilityPlot extends React.PureComponent<IProps, IState> {
-    constructor(props: IProps) {
-        super(props);
-        this.state = {
-            data: configureChartData(props.solution),
-        }
-    }
-
-    componentWillReceiveProps(nextProps: IProps) {
-        this.setState({
-            data: configureChartData(nextProps.solution)
-        });
-    }
-
     render() {
         return (
-            <BarChart width={600} height={300} data={this.state.data}>
-                <XAxis dataKey="name"/>
-                <YAxis/>
-                <Tooltip/>
-                <Legend />
-                <Bar dataKey="value" fill="#8884d8"/>
-            </BarChart>
+            <ParametersPlot solution={this.props.solution}/>
         );
     }
 }
+
--- src/client/pages/compartments/components/inverse-problem/inverse-problem-plot.tsx
+++ src/client/pages/compartments/components/inverse-problem/inverse-problem-plot.tsx
@@ -1,62 +1,18 @@
-import { safeGet } from '../../../../../lib/utils';
-import {IInverseProblemSolution, IInverseProblemOptions} from '../../../../redux/reducers/solvers/inverse-problem';
+import { ParametersPlot } from '../plots/ParametersPlot';
+import {IInverseProblemSolution} from '../../../../redux/reducers/solvers/inverse-problem';
 import * as React from 'react';
-import * as _ from 'lodash';
-const {BarChart, Bar, XAxis, YAxis, Tooltip, Legend} = require('recharts');
 
 interface IProps {
     solution: IInverseProblemSolution;
 }
 
 interface IState {
-    data: ISolution;
 }
-
-interface ISolutionData {
-    name: string;
-    value: number;
-}
-
-type ISolution = ISolutionData[];
-
-function getDefaultSolution(): ISolution {
-    return [{name: '', value: 0}];
-}
-
-function configureChartData(solution: IInverseProblemSolution): ISolution {
-    if (solution && solution.solution && _.keys(solution.solution).length) {
-        return _.map(safeGet(solution, x => x.solution), (value, key) => {
-            return {name: key, value};
-        });
-    }
-
-    return getDefaultSolution();
-}
-
 
 export class InverseProblemPlot extends React.PureComponent<IProps, IState> {
-    constructor(props: IProps) {
-        super(props);
-        this.state = {
-            data: configureChartData(props.solution),
-        }
-    }
-
-    componentWillReceiveProps(nextProps: IProps) {
-        this.setState({
-            data: configureChartData(nextProps.solution)
-        });
-    }
-
     render() {
         return (
-            <BarChart width={600} height={300} data={this.state.data}>
-                <XAxis dataKey="name"/>
-                <YAxis/>
-                <Tooltip/>
-                <Legend />
-                <Bar dataKey="value" fill="#8884d8"/>
-            </BarChart>
+            <ParametersPlot solution={this.props.solution}/>
         );
     }
 }
