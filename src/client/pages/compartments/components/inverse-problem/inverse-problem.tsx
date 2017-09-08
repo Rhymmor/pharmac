64a34f3581508638cc17d389f94b06bd6e186cb6
Anatoly Belonog <anatolybelonog@gmail.com>
2017-09-08 10:52:01 +0700

    refactor inverse problem component

--- src/client/pages/compartments/components/inverse-problem/inverse-problem.tsx
+++ src/client/pages/compartments/components/inverse-problem/inverse-problem.tsx
@@ -1,8 +1,22 @@
+import {
+    IInverseProblemOptions,
+    IInverseProblemSolution,
+    IInverseProblemStore
+} from '../../../../redux/reducers/solvers/inverse-problem';
+import { IParameters } from '../../../../redux/reducers/formulas';
+import { Modifier } from '../../../../utils/utils';
+import { ParamsBox } from '../paramsBox';
+import { safeGet } from '../../../../../lib/utils';
+import { ModelOptions } from '../ModelOptions';
+import { Box } from '../../../../components';
 import { InverseProblemPlot } from './inverse-problem-plot';
 import { AbstractProblem, IProblemProps } from '../abstract-problem';
 import * as React from 'react';
+import { Row, Col, Button } from 'react-bootstrap';
+import * as _ from 'lodash';
 
-interface IInverseProblemProps extends IProblemProps<any, any, any> {
+interface IInverseProblemProps extends IProblemProps<IInverseProblemSolution, IInverseProblemOptions, IInverseProblemStore> {
+    modifySyntheticParams: (modify: Modifier<IParameters>) => void;
 }
 
 interface IInverseProblemState {
@@ -10,17 +24,39 @@ interface IInverseProblemState {
 
 export class InverseProblem extends React.PureComponent<IInverseProblemProps, IInverseProblemState> {
     render() {
-        const {solve, problem, params, modifyParams, modifyOptions} = this.props;
+        const {solve, problem: {solution, options}, params, modifyParams, modifyOptions, modifySyntheticParams} = this.props;
         //Typescript type bug
         return (
-            <AbstractProblem
-                solve={solve}
-                params={params}
-                modifyParams={modifyParams}
-                modifyOptions={modifyOptions}
-                problem={problem}
-                Plot={InverseProblemPlot as any}
-            />
+            <Box className='direct-box'>    
+                <div>
+                    <ModelOptions 
+                        className='inline-block' 
+                        options={options as any} 
+                        modifyOptions={modifyOptions as any}
+                    />
+                    <Button onClick={solve} className='inline-block'>Solve</Button>
+                </div>
+                <Row>
+                    {
+                        !!_.keys(params).length &&
+                        <Col xs={6}>
+                            <ParamsBox label='Initial parameters' params={params} modifyParams={modifyParams}/>
+                        </Col>
+                    }
+                    {
+                        !!_.keys(params).length &&
+                        <Col xs={6}>
+                            <ParamsBox label='Synthetic parameters' params={options.syntheticParameters} modifyParams={modifySyntheticParams}/>
+                        </Col>
+                    }
+                    {
+                        !!safeGet(solution, x=>_.keys(x.solution).length) && 
+                        <Col xs={6}>
+                            <InverseProblemPlot solution={solution}/>
+                        </Col>
+                    }
+                </Row>
+            </Box>
         );
     }
 }
