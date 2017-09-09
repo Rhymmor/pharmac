942bd7e6b0c1a3d4a3888e8efaceaf229fdb3c8f
Anatoly Belonog <anatolybelonog@gmail.com>
2017-09-09 20:18:04 +0700

    fix server rendering

--- scripts/prod-server.js
+++ scripts/prod-server.js
@@ -5,9 +5,14 @@ const proxy = require('http-proxy-middleware');
 const port = process.env.PORT || 5000;
 const app = express();
 
-const staticPath = path.join(path.dirname(__dirname), 'browser-bundle');
+const projectPath = path.dirname(__dirname);
+
+const staticPath = path.join(projectPath, 'browser-bundle');
 app.use(express.static(staticPath));
-app.use('/api', proxy({target: 'http://localhost:3000'}))
+app.use('/api', proxy({target: 'http://localhost:3000'}));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(projectPath, '/browser-bundle', 'index.html'));
+});
 
 app.listen(port, (err) => {
   if (err) {
