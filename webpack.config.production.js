923cf30cc5d326b46e86506a01314660d057f678
Anatoly Belonog <anatolybelonog@gmail.com>
2017-10-28 02:24:53 +0700

    webpack prod optimization

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -3,18 +3,23 @@ var config = require('./webpack.config');
 var WebpackShellPlugin = require('webpack-shell-plugin');
 
 var frontend = config[0];
-
-
+frontend.devtool = 'cheap-module-source-map';
 // TODO: replace with tsc -> js -> babel rules
 frontend.plugins.push(
     new WebpackShellPlugin({
         onBuildEnd: ['sh scripts/compile-browser-bundle-js.sh']
+    }),
+    new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production')
+        }
     })
 );
 
 // async/await polyfill
 var vendor = frontend.entry.vendor;
 vendor.unshift('babel-polyfill')
+
 
 module.exports = config
 
