06a24b6e8eb3f3695a0b649df5359fde8fa3c70a
Anatoly Belonog <anatolybelonog@gmail.com>
2017-11-18 03:05:46 +0700

    create mongo connection on start server

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,4 +1,8 @@
+import "reflect-metadata";
 import { ConnectionOptions } from 'typeorm/connection/ConnectionOptions';
+import { Parameter } from './database/entity/parameter';
+import { Formula } from './database/entity/formula';
+import { Model } from './database/entity/model';
 import { logger, setGlobalLogger } from '../lib/logger';
 import { backendLogger } from './modules/backend-logger';
 import express = require('express');
@@ -6,6 +10,9 @@ import * as path from 'path';
 const bodyParser = require('body-parser');
 const compression = require('compression');
 import { router } from "./rest/router";
+import {createConnection} from "typeorm";
+import {join} from 'path';
+
 const connectionConfig: ConnectionOptions = {
     "type": "mongodb",
     "host": "localhost",
@@ -21,33 +28,36 @@ const connectionConfig: ConnectionOptions = {
 }
 
 async function main() {
-
     setGlobalLogger(backendLogger);
-    const app = express();
 
-    const project_root = process.cwd();
-    logger.info(`Root: ${project_root}`);
+    createConnection(connectionConfig).then(async connection => {
+        const app = express();
+    
+        const project_root = process.cwd();
+        logger.info(`Root: ${project_root}`);
+    
+        app.use(bodyParser.json({limit: '1024mb'})); // for parsing application/json
+        app.use(compression());
+    
+        // API
+        app.use(router);
+    
+        // serve webpack-packed resources
+        app.use('/', express.static(path.join(project_root, '/browser-bundle')));
+    
+        // serve static resources(like favicon)
+        app.use('/', express.static(path.join(project_root, '/public')));
+    
+        // send index.html for html5 router
+        app.get('*', (req, res) => {
+            res.sendFile(path.join(project_root, '/browser-bundle', 'index.html'));
+        });
+    
+        const port = '3000';
+        app.listen(port);
+        logger.warn(`Server started on http://localhost:${port}`);
 
-    app.use(bodyParser.json({limit: '1024mb'})); // for parsing application/json
-    app.use(compression());
-
-    // API
-    app.use(router);
-
-    // serve webpack-packed resources
-    app.use('/', express.static(path.join(project_root, '/browser-bundle')));
-
-    // serve static resources(like favicon)
-    app.use('/', express.static(path.join(project_root, '/public')));
-
-    // send index.html for html5 router
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(project_root, '/browser-bundle', 'index.html'));
-    });
-
-    const port = '3000';
-    app.listen(port);
-    logger.warn(`Server started on http://localhost:${port}`);
+    }).catch(error => logger.error(`TypeORM connection error: ${error}`));
 }
 
 main();
