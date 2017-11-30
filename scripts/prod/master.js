16c516935e90426383c9b3cfb73b76af6dbad252
Anatoly Belonog <anatolybelonog@gmail.com>
2017-11-30 22:36:43 +0700

    optimize prod memory using for heroku

--- scripts/prod/master.js
+++ scripts/prod/master.js
@@ -1,10 +1,11 @@
 "use strict";
 const cluster = require('cluster');
-const {cpus} = require('os');
+
+const workersNum = process.env.WORKERS_NUM || 1;
 
 function run_master() {
     if (!cluster.isMaster) { return; }
-    for (let i = 0; i < cpus().length; i++) {
+    for (let i = 0; i < workersNum; i++) {
         cluster.schedulingPolicy = cluster.SCHED_NONE;
         cluster.fork();
     }
